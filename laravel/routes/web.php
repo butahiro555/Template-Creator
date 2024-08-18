ef8953cd2476edcc1f469947130d0cba8360c698
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-08-18 15:59:05 +0900

    update routes/web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,54 +1,30 @@
 <?php
 
-use App\Http\Controllers\ProfileController;
+use App\Http\Controllers\TemplatesController;
 use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\AuthenticatedSessionController;
-use App\Http\Controllers\RegisteredUserController;
-use App\Http\Controllers\PasswordResetLinkController;
-use App\Http\Controllers\NewPasswordController;
 
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "web" middleware group. Make something great!
-|
-*/
+// 未ログインユーザーのページ
+Route::get('/', [TemplatesController::class, 'index']);
 
-Route::get('/', function () {
-    return view('welcome');
+// 認証済みユーザーのためのルートグループ
+Route::middleware(['auth'])->group(function () {
+    
+    // テンプレート関連のルート
+    Route::prefix('templates')->group(function () {
+        Route::get('/', [TemplatesController::class, 'show'])
+            ->name('templates.show');
+
+        Route::post('store', [TemplatesController::class, 'store'])
+            ->name('templates.store');
+
+        Route::put('update/{id}', [TemplatesController::class, 'update'])
+            ->name('templates.update');
+
+        Route::delete('destroy/{id}', [TemplatesController::class, 'destroy'])
+            ->name('templates.destroy');
+    });
+
+    // 検索機能
+    Route::get('search', [TemplatesController::class, 'search'])
+        ->name('search');
 });
-
-Route::get('/dashboard', function () {
-    return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
-
-Route::middleware('auth')->group(function () {
-    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
-
-// Authentication Routes
-Route::get('login', [AuthenticatedSessionController::class, 'create'])
-    ->name('login');
-Route::post('login', [AuthenticatedSessionController::class, 'store']);
-Route::post('logout', [AuthenticatedSessionController::class, 'destroy'])
-    ->name('logout');
-
-Route::get('register', [RegisteredUserController::class, 'create'])
-    ->name('register');
-Route::post('register', [RegisteredUserController::class, 'store']);
-
-Route::get('forgot-password', [PasswordResetLinkController::class, 'create'])
-    ->name('password.request');
-Route::post('forgot-password', [PasswordResetLinkController::class, 'store'])
-    ->name('password.email');
-
-Route::get('reset-password/{token}', [NewPasswordController::class, 'create'])
-    ->name('password.reset');
-Route::post('reset-password', [NewPasswordController::class, 'store'])
-    ->name('password.update');
