a398a0e93c86f670aedbb3435d8634c8bb771f74
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-08-18 16:01:45 +0900

    update RouteServiceProvider.php

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -17,7 +17,8 @@ class RouteServiceProvider extends ServiceProvider
      *
      * @var string
      */
-    public const HOME = '/dashboard';
+    /* dashboardのurlを使用せずに、indexにページ遷移させる。 */
+    public const HOME = '/';
 
     /**
      * Define your route model bindings, pattern filters, and other route configuration.
@@ -34,7 +35,10 @@ class RouteServiceProvider extends ServiceProvider
                 ->group(base_path('routes/api.php'));
 
             Route::middleware('web')
-                ->group(base_path('routes/web.php'));
+		    ->group(base_path('routes/web.php'));
+	   
+	    Route::middleware('web')
+		    ->group(base_path('routes/auth.php'));
         });
     }
 }
