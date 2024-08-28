0f2678f329ff554ee1c00eca702d62b2898b53bf
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-08-28 15:15:40 +0900

    update web.php

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -4,27 +4,33 @@ use App\Http\Controllers\TemplatesController;
 use Illuminate\Support\Facades\Route;
 
 // 未ログインユーザーのページ
-Route::get('/', [TemplatesController::class, 'index']);
+Route::get('/', [TemplatesController::class, 'index'])
+    ->name('templates.index');
 
 // 認証済みユーザーのためのルートグループ
 Route::middleware(['auth'])->group(function () {
     
-    // テンプレート関連のルート
-    Route::prefix('templates')->group(function () {
-        Route::get('/', [TemplatesController::class, 'show'])
-            ->name('templates.show');
+    // 認証済みユーザー専用のルート（メール認証済みユーザーのみ）
+    Route::middleware(['verified'])->group(function () {
 
-        Route::post('store', [TemplatesController::class, 'store'])
-            ->name('templates.store');
+        // テンプレート関連のルート
+        Route::prefix('templates')->group(function () {
+            Route::get('/', [TemplatesController::class, 'show'])
+                ->name('templates.show');
 
-        Route::put('update/{id}', [TemplatesController::class, 'update'])
-            ->name('templates.update');
+            Route::post('store', [TemplatesController::class, 'store'])
+                ->name('templates.store');
 
-        Route::delete('destroy/{id}', [TemplatesController::class, 'destroy'])
-            ->name('templates.destroy');
+            Route::put('update/{id}', [TemplatesController::class, 'update'])
+                ->name('templates.update');
+
+            Route::delete('destroy/{id}', [TemplatesController::class, 'destroy'])
+                ->name('templates.destroy');
+        });
+
+        // 検索機能
+        Route::get('search', [TemplatesController::class, 'search'])
+            ->name('search');
     });
 
-    // 検索機能
-    Route::get('search', [TemplatesController::class, 'search'])
-        ->name('search');
 });
