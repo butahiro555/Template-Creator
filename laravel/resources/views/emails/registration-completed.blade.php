5ba985f1344fb99f0c6c69ca8b5281bf6385f387
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-08-28 15:39:52 +0900

    add emails_view-files

---
+++ laravel/resources/views/emails/registration-completed.blade.php
@@ -0,0 +1,12 @@
+<!-- resources/views/emails/registration-completed.blade.php -->
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Registration Completed</title>
+</head>
+<body>
+    <h1>Registration Successful</h1>
+    <p>Your registration has been successfully completed.</p>
+</body>
+</html>
+
