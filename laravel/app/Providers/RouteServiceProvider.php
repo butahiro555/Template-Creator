da90dbf84daec04c88e2c89d138311eb001e7c46
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-08-28 14:27:48 +0900

    update RouteServiceProvider.php

--- laravel/app/Providers/RouteServiceProvider.php
+++ laravel/app/Providers/RouteServiceProvider.php
@@ -38,7 +38,7 @@ class RouteServiceProvider extends ServiceProvider
 		    ->group(base_path('routes/web.php'));
 	   
 	    Route::middleware('web')
-		    ->group(base_path('routes/auth.php'));
+		    ->group(base_path('routes/user.php'));
         });
     }
 }
