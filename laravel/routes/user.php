a5caf8959e9917f4a27e24fec2d1e5b56f97db6f
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-08-29 20:36:39 +0900

    change setting url and, method_name, route_name, add handle request and resend route

--- laravel/routes/user.php
+++ laravel/routes/user.php
@@ -9,8 +9,10 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('guest')->group(function () {
 
     // ユーザー仮登録
-    Route::get('register/temp', [TempUsersController::class, 'create'])->name('temp-user.create'); // ユーザー仮登録画面を表示
-    Route::post('register/temp', [TempUsersController::class, 'store'])->name('temp-user.store'); // ユーザー仮登録情報を送信
+    Route::get('register/index', [TempUsersController::class, 'tempRegister'])->name('temp-user.index'); // ユーザー仮登録画面を表示
+    Route::post('register/action', [TempUsersController::class, 'handleRequest'])->name('temp-user.handle'); //認証メール送信、再発行ボタンの処理を分ける
+    Route::post('register/temp/send', [TempUsersController::class, 'mailSend'])->name('temp-user.send'); // ユーザー仮登録情報を送信
+    Route::post('register/temp/resend', [TempUsersController::class, 'mailResend'])->name('temp-user.resend'); // 認証メール再発行
 
     // ユーザー本登録
 	Route::get('register/{token}', [UsersController::class, 'showRegistrationForm'])->name('register.show'); // トークンURLを持った専用ユーザー登録画面を表示
