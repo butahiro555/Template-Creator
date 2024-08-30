248af41761f6f4ed5de62eb24cdd2bd8f9fd12a3
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-08-30 13:41:23 +0900

    Create ValidationsController with email validation methods
    - Added methods for email format validation and existence check.
    - Implemented reusable validation logic for consistency across controllers.

--- laravel/app/Http/Controllers/TempUsersController.php
+++ laravel/app/Http/Controllers/TempUsersController.php
@@ -4,103 +4,91 @@ namespace App\Http\Controllers;
 
 use App\Models\TempUser;
 use App\Models\User;
+use App\Mail\VerificationEmail;
+use App\Http\Controllers\ValidationController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Str;
 use Illuminate\Support\Facades\Mail;
 use Carbon\Carbon;
-use App\Mail\VerificationEmail;
 
 class TempUsersController extends Controller
 {   
+    // メール認証送信画面を表示
     public function tempRegister()
     {
         return view('auth.temp-register');
     }
 
-    // 認証メール送信と、再発行の仕分け
+    // sendアクションと、resendアクションの処理の分岐
     public function handleRequest(Request $request)
     {
         $action = $request->input('action');
 
-        if ($action === 'send_verification') {
-            return $this->send($request);
-        } elseif ($action === 'resend_verification') {
-            return $this->resend($request);
+        switch ($action) {
+            case 'send_verification':
+                return $this->send($request);
+            case 'resend_verification':
+                return $this->resend($request);
+            default:
+                return redirect()->back()->withErrors(['action' => 'Invalid action specified.']);
         }
-
-        return redirect()->back()->withErrors(['action' => 'Invalid action specified.']);
     }
 
-    // 認証コード発行
+    // メール認証送信
     public function send(Request $request)
     {
-        if ($request->input('action') === 'send_verification') {
-            $request->validate([
-                'email' => ['required', 'string', 'email', 'max:255'],
-            ]);
+        $this->validateEmail($request);
 
-            // 入力されたメールアドレスが、登録済みのメールアドレスなのかを確認する
-            $checkRegisteredTempUsers = TempUser::where('email', $request->email)->first();
-            $checkRegisteredUsers = User::where('email', $request->email)->first();
+        // 入力されたメールアドレスが、登録済みかを確認する
+        $checkRegisteredTempUsers = TempUser::where('email', $request->email)->first();
+        $checkRegisteredUsers = User::where('email', $request->email)->first();
 
-            // 仮登録済みのメッセージ
-            if($checkRegisteredTempUsers)
-            {
-                return redirect()->back()->withErrors(['email' => 'Please confirm you email or resend verify code.']);
-            }
-
-            // 本登録済みのメッセージ
-            if($checkRegisteredUsers)
-            {
-                return redirect()->back()->withErrors(['email' => 'This email address is already registered.']);
-            }
-
-            // メールアドレス宛に認証コードとトークンを発行して送信
-            $tempUser = TempUser::createOrUpdateTempUser($request->email);
-            $verificationCode = $tempUser->verification_code;
-            $token = $tempUser->token;
-            $verificationUrl = route('register.show', ['token' => $token]);
-
-            session(['verification_code' => $verificationCode, 'email' => $request->email, 'token' => $token]);
-
-            Mail::to($request->email)->send(new VerificationEmail($verificationUrl, $verificationCode));
-
-            return view('auth.verify-email');
+        // 認証送信済みかをチェック
+        if ($checkRegisteredTempUsers) {
+            return redirect()->back()->withErrors(['email' => 'Please confirm your email or resend verify code.']);
+        }
+        
+        // ユーザー登録済みかをチェック
+        if ($checkRegisteredUsers) {
+            return redirect()->back()->withErrors(['email' => 'This email address is already registered.']);
         }
 
-        return redirect()->back()->withErrors(['action' => 'Invalid action specified.']);
+        // 最下部のメソッドを呼び出し
+        return $this->processVerification($request);
     }
 
-    // 認証コード再発行
+    // メール認証再送信
     public function resend(Request $request)
     {
-        if ($request->input('action') === 'resend_verification') {
-            $request->validate([
-                'email' => ['required', 'string', 'email', 'max:255'],
-            ]);
+        $this->validateEmail($request);
 
-            // 仮ユーザーを検索
-            $tempUser = TempUser::where('email', $request->email)->first();
+        // 仮ユーザーを検索
+        $tempUser = TempUser::where('email', $request->email)->first();
 
-            // 仮ユーザー登録のアドレスがなければ、メッセージを返す
-            if (!$tempUser) {
-                return redirect()->back()->withErrors(['email' => 'Email address not found.']);
-            }
-            
-            // 仮ユーザー宛てに認証コードとトークンを再発行して送信
-            $tempUser = TempUser::createOrUpdateTempUser($request->email);
-            $verificationCode = $tempUser->verification_code;
-            $token = $tempUser->token;
-            $verificationUrl = route('register.show', ['token' => $token]);
-
-            session(['verification_code' => $verificationCode, 'email' => $request->email, 'token' => $token]);
-
-            Mail::to($request->email)->send(new VerificationEmail($verificationUrl, $verificationCode));
-
-            return view('auth.verify-email');
+        // 仮ユーザー登録済みかをチェック
+        if (!$tempUser) {
+            return redirect()->back()->withErrors(['email' => 'Email address not found.']);
         }
 
-        return redirect()->back()->withErrors(['action' => 'Invalid action specified.']);
+        return $this->processVerification($request);
+    }
+
+    // 仮ユーザー情報のバリデーションチェックおよび、メール送信
+    protected function processVerification(Request $request)
+    {   
+        // メールアドレス宛に認証コードとトークンを発行
+        $tempUser = TempUser::createOrUpdateTempUser($request->email);
+        $verificationCode = $tempUser->verification_code;
+        $token = $tempUser->token;
+        $verificationUrl = route('register.show', ['token' => $token]);
+
+        // セッションに認証コードと、トークンを保存
+        session(['verification_code' => $verificationCode, 'email' => $request->email, 'token' => $token]);
+
+        // メール送信
+        Mail::to($request->email)->send(new VerificationEmail($verificationUrl, $verificationCode));
+
+        // メール確認を促すビューファイル
+        return view('auth.verify-email');
     }
 }
-
