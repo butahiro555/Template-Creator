cebe88fb1ad19ac9a61b796d736db380f84299fc
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-01 15:52:21 +0900

    Additional scheduling functions
    - The command temp-users:clean-expired is executed every minute.

--- laravel/app/Console/Kernel.php
+++ laravel/app/Console/Kernel.php
@@ -2,6 +2,7 @@
 
 namespace App\Console;
 
+use App\Console\Commands\CleanExpiredTempUsers; // コマンドクラスのインポート
 use Illuminate\Console\Scheduling\Schedule;
 use Illuminate\Foundation\Console\Kernel as ConsoleKernel;
 
@@ -12,7 +13,7 @@ class Kernel extends ConsoleKernel
      */
     protected function schedule(Schedule $schedule): void
     {
-        // $schedule->command('inspire')->hourly();
+        $schedule->command('temp-users:clean-expired')->everyMinute(); // 毎分実行
     }
 
     /**
