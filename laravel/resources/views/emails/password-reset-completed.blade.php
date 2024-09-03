c9c745f41ca3889f48437f9069ef297a52e29078
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-03 15:58:21 +0900

    Create password reset completed confirmation view
    - Added a simple HTML view to confirm successful password reset
    - Includes a message about the successful reset and contact instructions

---
+++ laravel/resources/views/emails/password-reset-completed.blade.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <meta charset="utf-8">
+    <title>Password Reset Completed</title>
+</head>
+<body>
+    <h1>Password Reset Successful</h1>
+    <p>Your password has been successfully reset.</p>
+    <p> If you did not request this change, please contact our support team immediately.</p>
+    <p>Thank you for using our service!</p>
+</body>
+</html>
+
