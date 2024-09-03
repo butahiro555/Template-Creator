7885f3324f61650a61c52b699dcf0e66b0492f66
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-03 15:39:59 +0900

    Add PasswordResetMail class for password reset notifications
    - Implemented PasswordResetMail class to handle password reset email functionality
    - Added `verificationUrl` and `verificationCode` properties to the class
    - Configured email subject and content view (`emails.password-reset`)
    - Passed `verificationCode` and `verificationUrl` to the email view

---
+++ laravel/app/Mail/PasswordResetMail.php
@@ -0,0 +1,62 @@
+<?php
+
+namespace App\Mail;
+
+use Illuminate\Bus\Queueable;
+use Illuminate\Contracts\Queue\ShouldQueue;
+use Illuminate\Mail\Mailable;
+use Illuminate\Mail\Mailables\Content;
+use Illuminate\Mail\Mailables\Envelope;
+use Illuminate\Queue\SerializesModels;
+
+class PasswordResetMail extends Mailable
+{
+    use Queueable, SerializesModels;
+
+    public $verificationUrl;
+    public $verificationCode;
+
+    /**
+     * Create a new message instance.
+     */
+    public function __construct($verificationUrl, $verificationCode)
+    {
+        $this->verificationUrl = $verificationUrl;
+        $this->verificationCode = $verificationCode;
+    }
+
+    /**
+     * Get the message envelope.
+     */
+    public function envelope(): Envelope
+    {
+        return new Envelope(
+            subject: 'Password Reset Mail',
+        );
+    }
+
+    /**
+     * Get the message content definition.
+     */
+    public function content(): Content
+    {
+        return new Content(
+            view: 'emails.password-reset', // ビューの名前を指定
+            with: [
+                'verificationCode' => $this->verificationCode,
+                'verificationUrl' => $this->verificationUrl
+            ],
+        );
+    }
+
+    /**
+     * Get the attachments for the message.
+     *
+     * @return array<int, \Illuminate\Mail\Mailables\Attachment>
+     */
+    public function attachments(): array
+    {
+        return [];
+    }
+}
+
