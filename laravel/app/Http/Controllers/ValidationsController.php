af44544ef5b743a844ab521650dba9ae493ebc8e
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-04 20:46:52 +0900

    Add some methods for Validation check
    - added validateName method for validation users.
    - added validatePassword method for validation passwords.
    - added validateAuthInputs method for emails and verification codes and passwords.
    - added validateRegisterForm method for emails and verification codes and names and passwords.
    - added validateTemplate method for titles and contents.

--- laravel/app/Http/Controllers/ValidationsController.php
+++ laravel/app/Http/Controllers/ValidationsController.php
@@ -6,11 +6,58 @@ use Illuminate\Http\Request;
 
 class ValidationsController extends Controller
 {
-    // メールバリデーション用メソッド
+    // メール用バリデーションチェック
     public function validateEmail(Request $request)
     {
         return $request->validate([
             'email' => ['required', 'string', 'email', 'max:255'],
         ]);
     }
+
+    // ユーザーネーム用バリデーションチェック
+    public function validateName(Request $request)
+    {
+        return $request->validate([
+            'name' => ['required', 'string', 'max:255'],
+        ]);
+    }
+
+    // パスワード用バリデーションチェック
+    public function validatePassword(Request $request)
+    {
+        return $request->validate([
+            'password' => ['required', 'string', 'confirmed', 'min:8'],
+        ]);
+    }
+
+    // メール、認証コード、パスワードのバリデーションチェック
+    public function validateAuthInputs(Request $request)
+    {
+        return $request->validate([
+            'email' => ['required', 'email'],
+            'verification_code' => ['required', 'string', 'size:5'],
+            'password' => ['required', 'string', 'confirmed', 'min:8'],
+        ]);
+    }
+
+    // ユーザー登録フォーム用バリデーションチェック
+    public function validateRegisterForm(Request $request)
+    {
+        return $request->validate([
+            'email' => ['required', 'email'],
+            'verification_code' => ['required', 'string', 'size:5'],
+            'name' => ['required', 'string', 'max:255'],
+            'password' => ['required', 'string', 'confirmed', 'min:8'],
+        ]);
+    }
+
+    // テンプレート用バリデーションチェック
+    public function validateTemplate(Request $request)
+    {
+        return $request->validate([
+            'title' => 'required|max:20',
+            'content' => 'required|max:191',
+        ]);
+    }
 }
+
