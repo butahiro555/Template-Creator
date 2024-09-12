182e7c9107a0d3d03784307be23800b3f163fe9a
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-12 22:10:33 +0900

    Add routes for user profile management
    - Added profile viewing, username update, and password update functionalities using ProfileController
    - Added user account deletion functionality
    - Added "goodbye" page for redirection after account deletion

--- laravel/routes/user.php
+++ laravel/routes/user.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\TempUsersController;
 use App\Http\Controllers\UsersController;
 use App\Http\Controllers\AuthsController;
+use App\Http\Controllers\ProfileController;
 use App\Http\Controllers\ForgotPasswordUsersController;
 use Illuminate\Support\Facades\Route;
 
@@ -16,7 +17,7 @@ Route::middleware('guest')->group(function () {
     Route::post('register/temp/resend', [TempUsersController::class, 'mailResend'])->name('temp-user.resend'); // 認証メール再発行
 
     // ユーザー本登録
-	Route::get('register/{token}', [UsersController::class, 'showRegistrationForm'])->name('register.show'); // トークンURLを持った専用ユーザー登録画面を表示
+    Route::get('register/{token}', [UsersController::class, 'showRegistrationForm'])->name('register.show'); // トークンURLを持った専用ユーザー登録画面を表示
     Route::post('register', [UsersController::class, 'register'])->name('register'); // ユーザー登録情報を送信
 
     // ログイン
@@ -28,10 +29,34 @@ Route::middleware('guest')->group(function () {
     Route::post('forgot-password/send', [ForgotPasswordUsersController::class, 'sendResetForUser'])->name('forgot-password.send'); // 登録メールアドレス宛にパスワード再設定リンクURLを送信
     Route::get('forgot-password/resetform', [ForgotPasswordUsersController::class, 'showResetForm'])->name('forgot-password.resetform'); // トークンURLを持った専用ユーザーパスワードリセット画面を表示
     Route::post('forgot-password/reset', [ForgotPasswordUsersController::class, 'passwordReset'])->name('forgot-password.reset'); // 再設定パスワードを送信
+
+    // 退会後のリダイレクト先
+    Route::get('goodbye', function () {
+        return view('goodbye');
+    })->name('goodbye');
 });
 
 // 認証されたユーザーにアクセスを許可するためのルートグループ
 Route::middleware('auth')->group(function () {
+
+    // プロフィール一覧
+    Route::get('profile', [ProfileController::class, 'showProfile'])->name('profile.index');
+
+    // ユーザー名変更機能
+    Route::get('profile/edit-name/form', [ProfileController::class, 'showEditNameForm'])->name('profile.edit-name'); // ユーザー名変更画面を表示
+    Route::patch('profile/edit-name/send', [ProfileController::class, 'updateNameSend'])->name('profile.update-name');
+
+    // パスワード変更機能
+    Route::get('profile/edit-password/confirm-password', [ProfileController::class, 'confirmCurrentPasswordForm'])->name('profile.password-confirm-form'); // 現パスワード入力フォームの表示
+    Route::post('profile/edit-password/confirm-password', [ProfileController::class, 'confirmCurrentPassword'])->name('profile.password-confirm'); // 現パスワードの確認処理
+    Route::match(['get', 'post'], 'profile/edit-password', [ProfileController::class, 'showEditPasswordForm'])->name('profile.edit-password'); // パスワード変更フォームの表示
+    Route::patch('profile/edit-password/send', [ProfileController::class, 'updatePasswordSend'])->name('profile.update-password');
+
+    // ユーザー退会機能
+    Route::get('profile/delete-user/confirm-password', [ProfileController::class, 'confirmCurrentPasswordFormForDeleteUser'])->name('profile.delete-user-password-confirm-form'); // 現在のパスワード入力フォーム
+    Route::post('profile/delete-user/confirm-password', [ProfileController::class, 'confirmCurrentPasswordForDeleteUser'])->name('profile.delete-user-password-confirm'); // 現パスワードの確認処理
+    Route::match(['get', 'post'], 'profile/delete-user/form', [ProfileController::class, 'deleteUserForm'])->name('profile.delete-user-form'); // 退会確認画面
+    Route::delete('profile/delete-user/form', [ProfileController::class, 'destroy'])->name('profile.delete-user-send');
 
     // ログアウト
     Route::post('logout', [AuthsController::class, 'logout'])->name('logout');
