10c03933b4fe45765c44e4c68aa7146019d71a02
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-17 12:14:26 +0900

    Refactor base layout for improved styling and script management
    - Updated meta tags for better viewport control and charset.
    - Upgraded Bootstrap and FontAwesome versions for updated styles.
    - Reorganized layout to include status and error messages with fixed height area.
    - Removed inline JavaScript and updated external script references.
    - Improved script loading with latest versions of jQuery, Popper.js, and Bootstrap.

--- laravel/resources/views/layouts/app.blade.php
+++ laravel/resources/views/layouts/app.blade.php
@@ -1,63 +1,44 @@
 <!DOCTYPE html>
 <html lang="ja">
-    <head>
-        <meta charset="utf-8">
-        <title>Template&nbsp;Creator</title>
-        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/css/bootstrap.min.css">
-        <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.5.0/css/all.css">
-        <link rel="stylesheet" href="{{ asset('css/style.css') }}">
-        <meta name="viewport" content="width=device-width,initial-scale=0.7">
-    </head>
-    <body>
-        @include('commons.navbar')
-        
-        <div class="container">
-            @include('commons.error_templates')
-            
-            @yield('content')
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Template Creator</title>
+   
+    <!-- Bootstrap CSS -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css">
+    
+    <!-- FontAwesome CSS -->
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.2/css/all.css">
+    
+    <!-- Bootstrap Icons CSS -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bootstrap-icons/1.10.5/font/bootstrap-icons.min.css">
+    
+    <!-- Custom CSS -->
+    <link rel="stylesheet" href="{{ asset('css/style.css') }}">
+</head>
+<body>
+    <div class="mt-3 container">
+        <!-- エラーメッセージの領域の高さを固定してスペースを確保 -->
+        <div style="height: 10px;">
+            @include('commons.status_message')
+            @include('commons.error_message')
         </div>
-        
-        <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js"></script>
-        <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js"></script>
-        <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js"></script>
-        <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
-        
-        <!-- クリップボードにコピーするためのjavascript -->
-        <script>
-        function copyToClipboard()
-        {
-            // コピー対象をJavaScript上で変数として定義する
-            var copyTarget = document.getElementById("copyTarget");
+        @yield('content')
+    </div>
 
-            // コピー対象のテキストを選択する
-            copyTarget.select();
-
-            // 選択しているテキストをクリップボードにコピーする
-            document.execCommand("Copy");
-
-            // コピーをお知らせする
-            alert("Copy Completed!" );
-        }
-        </script>
-        
-        <!-- deleteのときのメッセージ -->
-        <script>
-        $('#delete').click(function(){
-                if(!confirm('Do you really want to delete this?')){
-                    /* キャンセルの時の処理 */
-                    return false;
-                }
-            });
-        </script>
-        
-        <!-- updateのときのメッセージ -->
-        <script>
-        $('#warning').click(function(){
-                if(!confirm('Do you really want to overwrite save this?')){
-                    /* キャンセルの時の処理 */
-                    return false;
-                }
-            });
-        </script>
-    </body>
+    <!-- jQuery -->
+    <script src="https://code.jquery.com/jquery-3.6.0.min.js"></script>
+    
+    <!-- Popper.js (Bootstrap 4.5.x needs Popper.js 1.x) -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.0/dist/umd/popper.min.js"></script>
+    
+    <!-- Bootstrap JS -->
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
+    
+    <!-- FontAwesome JS -->
+    <script defer src="https://use.fontawesome.com/releases/v5.7.2/js/all.js"></script>
+    
+    @yield('scripts')
+</body>
 </html>
