7c61851a8a280383dd737ab6237406794b0088ad
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-23 15:09:44 +0900

    Create success_message.php and add Japanese success messages
    - Created success_message.php file for success messages
    - Added Japanese translations for user registration, name change, password change, and password reset success messages

---
+++ laravel/resources/lang/ja/success_message.php
@@ -0,0 +1,8 @@
+<?php
+
+return [
+	'registered_successful' => 'ユーザー登録が完了しました！',
+	'name_change_successful' => 'ユーザー名の変更が完了しました！',
+	'password_change_successful' => 'パスワードの変更が完了しました！',
+	'password_reset_successful' => 'パスワードのリセットが完了しました！'
+];
