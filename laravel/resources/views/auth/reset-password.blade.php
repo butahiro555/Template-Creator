ec175ffd2d338da1e15b8f9004bfac9f4e2bd933
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-24 13:53:52 +0900

    Removed error message displays in the reset password view for the following fields:
    - "email"
    - "verification_code"
    - "password"
    - "password_confirmation"

--- laravel/resources/views/auth/reset-password.blade.php
+++ laravel/resources/views/auth/reset-password.blade.php
@@ -11,22 +11,18 @@
                 <div class="mb-3 mt-3">
                     <x-input-label for="email" :value="__('Email')" />
                     <x-text-input id="email" class="form-control" type="email" name="email" :value="old('email') ?? $email" required readonly />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
                 </div>
                 <div class="mb-3">
                     <x-input-label for="verification_code" :value="__('Verification Code')" />
                     <x-text-input id="verification_code" class="form-control" type="text" name="verification_code" required />
-                    <x-input-error :messages="$errors->get('verification_code')" class="mt-2 text-danger" />
                 </div>
                 <div class="mb-3">
                     <x-input-label for="password" :value="__('New Password')" />
                     <x-text-input id="password" class="form-control" type="password" name="password" required autocomplete="new-password" />
-                    <x-input-error :messages="$errors->get('password')" class="mt-2 text-danger" />
                 </div>
                 <div class="mb-3">
                     <x-input-label for="password_confirmation" :value="__('Confirm New Password')" />
                     <x-text-input id="password_confirmation" class="form-control" type="password" name="password_confirmation" required autocomplete="new-password" />
-                    <x-input-error :messages="$errors->get('password_confirmation')" class="mt-2 text-danger" />
                 </div>
 		        <div class="d-flex justify-content-end align-items-center mt-4">
                     <x-primary-button class="btn btn-info" style="width: auto;">
