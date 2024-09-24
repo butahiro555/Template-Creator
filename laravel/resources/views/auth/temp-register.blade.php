b0978fc2cb738272dc22331aa4a800c29d238c63
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-24 13:54:30 +0900

    Removed error message displays in the temporary registration view for the following fields:
    - "verification"
    - "email"

--- laravel/resources/views/auth/temp-register.blade.php
+++ laravel/resources/views/auth/temp-register.blade.php
@@ -6,17 +6,11 @@
             <div class="text-center">
                 <h1>Temporary Registration</h1>
             </div>
-            @if ($errors->has('verification'))
-                <div class="alert alert-danger">
-                    {{ $errors->first('verification') }}
-                </div>
-            @endif
             <form id="formClear" action="{{ route('temp-user.handle') }}" method="POST">
                 @csrf
                 <div class="mb-3">
                     <x-input-label for="email" :value="__('Email')" />
                     <x-text-input id="email" class="form-control" type="email" name="email" required />
-                    <x-input-error :messages="$errors->get('email')" class="mt-2 text-danger" />
                 </div>               
                 <div class="text-right">
                     <x-primary-button class="btn btn-success" name="action" value="send_verification">
