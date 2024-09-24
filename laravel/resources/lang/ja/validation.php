37470b514aa0d4657317d067a9b3c70b9ced09f1
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-09-24 14:32:30 +0900

    Updated setup procedures.

--- laravel/resources/lang/ja/validation.php
+++
@@ -1,8 +0,0 @@
-<?php
-
-return [
-    'verification_invalid' => '無効または期限切れの検証情報です。',
-    'token_expired' => 'トークンの有効期限が切れています。',
-    'verification_code_expired' => '検証コードの有効期限が切れています。',
-    'verification_code_incorrect' => '検証コードが正しくありません。',
-];
