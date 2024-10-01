9cf84bb17258dc01ac08a3709a8b740e1e068793
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-10-01 20:18:56 +0900

    Update email subjects to Japanese in multiple mail classes

--- laravel/app/Mail/PasswordResetMail.php
+++ laravel/app/Mail/PasswordResetMail.php
@@ -31,7 +31,7 @@ class PasswordResetMail extends Mailable
     public function envelope(): Envelope
     {
         return new Envelope(
-            subject: 'Password Reset Mail',
+            subject: '[Template Creator]パスワードリセット',
         );
     }
 
@@ -59,4 +59,3 @@ class PasswordResetMail extends Mailable
         return [];
     }
 }
-
