294273745f3072b2891dae729979895f85d66a93
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-10-04 16:58:34 +0900

    Reorganize success messages alphabetically for consistency
    - Sorted success messages in success_message.php file alphabetically to improve clarity and maintainability.
    - Added section headers for better organization of messages related to name changes, password changes, and user registration.

--- laravel/resources/lang/ja/success_message.php
+++ laravel/resources/lang/ja/success_message.php
@@ -1,8 +1,13 @@
 <?php
 
 return [
-	'registered_successful' => 'ユーザー登録が完了しました！',
+	// N
 	'name_change_successful' => 'ユーザー名の変更が完了しました！',
+
+	// P
 	'password_change_successful' => 'パスワードの変更が完了しました！',
-	'password_reset_successful' => 'パスワードのリセットが完了しました！'
+	'password_reset_successful' => 'パスワードのリセットが完了しました！',
+
+	// R
+	'registered_successful' => 'ユーザー登録が完了しました！',
 ];
