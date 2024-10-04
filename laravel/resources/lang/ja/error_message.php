49c79606cee396baa8940aa802accaffc3fdef66
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-10-04 16:57:30 +0900

    Reorganize error messages alphabetically and enhance message clarity
    - Sorted error messages in error_message.php file alphabetically for better readability and management.
    - Added new error messages, including those for verification codes and email confirmations.
    - Improved existing messages for clarity, ensuring users receive clear guidance when encountering errors.

--- laravel/resources/lang/ja/error_message.php
+++ laravel/resources/lang/ja/error_message.php
@@ -1,21 +1,47 @@
 <?php
 
 return [
+    // A
+    'already_send' => '既に確認メールが送信されていますので、メールボックスを確認してください。',
+
+    // C
+    'confirmed' => 'パスワードの確認が一致しません。',
+    'confirm_or_resend_your_email' => 'メールボックスを確認するか、認証メールを再送信してください。',
+    'custom' => [
+        'title' => [
+            'max' => 'タイトルに使用できる文字数は20字までです。',
+        ],
+        'content' => [
+            'max' => '本文に使用できる文字数は191字までです。',
+        ],
+    ],
+
+    // E
+    'email_invalid' => 'メールアドレスか、パスワードが間違っています。',
+
+    // N
+    'name_not_change' => 'ユーザー名が変更されていません。',
+
+    // P
+    'password_not_change' => 'パスワードが変更されていません。',
+    'password_is_wrong' => 'パスワードが正しくありません。',
+
+    // R
+    'required' => '入力欄が空のままです。文字を入力してください。',
+    'resend_limit' => '認証メール再送信の上限回数に達しました。',
+
+    // T
+    'template_not_found' => 'テンプレートが見つかりません。',
+    'token_expired' => 'トークンの有効期限が切れています。',
+
+    // U
+    'unauthorezed_access' => 'アクセスが許可されていません。',
+    'unexpected_error' => '予期しないエラーが発生しました。もう一度お試しください。',
     'user_not_found' => 'ユーザー情報が見つかりません。',
     'user_already_registered' => '既に登録済みのユーザーです。',
-    'email_invalid' => 'メールアドレスか、パスワードが間違っています。',
-    'password_is_wrong' => 'パスワードが正しくありません。',
-    'confirm_or_resend_your_email' => 'メールボックスを確認するか、認証メールを再送信してください。',
-    'resend_limit' => '認証メール再送信の上限回数に達しました。',
-    'verification_invalid' => '無効または期限切れの検証情報です。',
-    'token_expired' => 'トークンの有効期限が切れています。',
+
+    // V
     'verification_code_expired' => '検証コードの有効期限が切れています。',
     'verification_code_incorrect' => '検証コードが正しくありません。',
-    'confirmed' => 'パスワードの確認が一致しません。',
-    'name_not_change' => 'ユーザー名が変更されていません。',
-    'password_not_change' => 'パスワードが変更されていません。',
-    'unauthorezed_access' => 'アクセスが許可されていません。',
-    'template_not_found' => 'テンプレートが見つかりません。',
-    'unexpected_error' => '予期しないエラーが発生しました。もう一度お試しください。',
+    'verification_invalid' => '無効または期限切れの検証情報です。',
 ];
-
