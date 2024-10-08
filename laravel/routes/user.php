cad50fa767525c99c34dd3513f18cf84d15a3eac
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-10-08 19:27:26 +0900

    Refactor user registration routes for clarity and improved function naming
    - Removed obsolete route for handling email actions.
    - Updated send route to use the new sendVerificationEmail method.
    - Improved route comments for better understanding.

--- laravel/routes/user.php
+++ laravel/routes/user.php
@@ -12,9 +12,7 @@ Route::middleware('guest')->group(function () {
 
     // ユーザー仮登録
     Route::get('register/index', [TempUsersController::class, 'tempRegister'])->name('temp-user.index'); // ユーザー仮登録画面を表示
-    Route::post('register/action', [TempUsersController::class, 'handleRequest'])->name('temp-user.handle'); //認証メール送信、再発行ボタンの処理を分ける
-    Route::post('register/temp/send', [TempUsersController::class, 'mailSend'])->name('temp-user.send'); // ユーザー仮登録情報を送信
-    Route::post('register/temp/resend', [TempUsersController::class, 'mailResend'])->name('temp-user.resend'); // 認証メール再発行
+    Route::post('register/temp/send', [TempUsersController::class, 'sendVerificationEmail'])->name('temp-user.send'); // ユーザー仮登録情報を送信
 
     // ユーザー本登録
     Route::get('register/{token}', [UsersController::class, 'showRegistrationForm'])->name('register.show'); // トークンURLを持った専用ユーザー登録画面を表示
