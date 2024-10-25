ae92a7f307e5ce1a00083f63c20aa9ace71a31fd
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-10-25 15:24:17 +0900

    Update layouts

--- resources/views/auth/temp-register.blade.php
+++ resources/views/auth/temp-register.blade.php
@@ -2,25 +2,27 @@
 
 @section('content')
     <div class="d-flex justify-content-center align-items-center" style="min-height: 85vh;">
-        <div class="rounded border p-4 shadow-sm" style="max-width: 600px; width: 100%;">
-            <div class="text-center">
+        <div class="rounded border p-2 shadow-sm" style="max-width: 600px; width: 100%;">
+            <div class="text-center mt-2">
                 <h2>仮ユーザー登録画面</h2>
             </div>
             <form id="formClear" action="{{ route('temp-user.send') }}" method="POST">
                 @csrf
-                <div class="mb-3">
+                <div class="mb-3 p-2">
                     <x-input-label for="email" :value="__('メールアドレス')" />
                     <x-text-input id="email" class="form-control" type="email" name="email" required />
                 </div>               
-                <div class="text-right">
-                    <x-primary-button class="btn btn-success" value="send_verification">
-                        {{ __('認証コード発行') }}
-                    </x-primary-button>
-                    <x-primary-button type="button" class="btn btn-secondary ml-2" onclick="window.location.href='{{ route('templates.index') }}'">
-                        {{ __('キャンセル') }}
-                    </x-primary-button>
+                <div class="d-flex justify-content-end align-items-center mt-4">
+                    <div class="d-flex">
+                        <x-primary-button class="btn btn-success" style="width: 170px;" value="send_verification">
+                            {{ __('認証コード発行') }}
+                        </x-primary-button>
+                        <x-primary-button type="button" class="btn btn-secondary ml-1" style="width: 140px;" onclick="window.location.href='{{ route('templates.index') }}'">
+                            {{ __('キャンセル') }}
+                        </x-primary-button>
+                    </div>
                 </div>
-            </form>
+             </form>
         </div>
     </div>
 @endsection
