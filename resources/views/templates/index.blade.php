54d89fc312f767f750b875914616daea011c5162
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-10-25 17:52:10 +0900

    Update layouts

--- resources/views/templates/index.blade.php
+++ resources/views/templates/index.blade.php
@@ -6,30 +6,24 @@
     <div class="top">
         <form action="{{ route('templates.store') }}" method="POST">
             @csrf
-            <div class="container">
-                @if(Auth::check())
-                    <div class="form-group">
-                        <input type="text" name="title" class="form-control" placeholder="タイトル" required>
-                    </div>
-                    <div class="form-group">
-                        <textarea id="copyTarget" name="content" rows="13" class="form-control" placeholder="保存、またはコピーしたい文章を入力してください。"></textarea>
-                    </div>
-                @else
-                    <div class="form-group">
-                        <textarea id="copyTarget" name="content" rows="13" class="form-control" placeholder="ユーザー登録をすることで、様々な機能を利用することが可能です。"></textarea>
-                    </div>
-                @endif
-                
-                <div class="d-flex justify-content-end">
-                    @if(Auth::check())
-                        <div class="wrapper-button mr-2">
-                            <button type="submit" class="btn btn-primary">保存</button>
-                        </div>
-                    @endif
-                    <div class="wrapper-button">
-                        <button type="button" onclick="copyToClipboard('copyTarget')" class="btn btn-info">コピー</button>
-                    </div>
-                </div>
+            @if(Auth::check())
+            <div class="form-group">
+                <input type="text" name="title" class="form-control" placeholder="タイトル" required>
+            </div>
+            <div class="form-group">
+                <textarea id="copyTarget" name="content" rows="13" class="form-control" placeholder="保存、またはコピーしたい文章を入力してください。"></textarea>
+            </div>
+            @else
+            <div class="form-group">
+                <textarea id="copyTarget" name="content" rows="13" class="form-control" placeholder="ユーザー登録をすることで、様々な機能を利用することが可能です。"></textarea>
+            </div>
+            @endif
+        
+            <div class="d-flex justify-content-end">
+            @if(Auth::check())
+                <button type="submit" class="btn btn-primary mr-2">保存</button>
+            @endif
+                <button type="button" onclick="copyToClipboard('copyTarget')" class="btn btn-info">コピー</button>
             </div>
         </form>
     </div>
