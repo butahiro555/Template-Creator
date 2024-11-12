479f5adbed8f819ed55fb36fde2de86e0186d5a5
butahiro555 <tomohiro.taniguchi1230@gmail.com>
2024-11-12 15:56:10 +0900

    Add test cases for login form display, successful login, and incorrect password handling.
    - Added tests for displaying the login page.
    - Added test to verify redirection upon successful login.
    - Added test to handle incorrect password scenarios.
    - Added test to ensure unauthenticated users are redirected to the login page when accessing /templates.
    - Added test to confirm that verified users can access /templates successfully.

--- tests/Feature/Auth/AuthenticationTest.php
+++ tests/Feature/Auth/AuthenticationTest.php
@@ -3,12 +3,14 @@
 namespace Tests\Feature\Auth;
 
 use App\Models\User;
-use Tests\TestCase;
+use App\Http\Middleware\Authenticate;
 use App\Providers\RouteServiceProvider;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Support\Facades\Notification;
 use Illuminate\Support\Facades\Session;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Facades\Route;
+use Tests\TestCase;
 
 class AuthenticationTest extends TestCase
 {
@@ -29,6 +31,7 @@ class AuthenticationTest extends TestCase
         $this->withoutMiddleware([\App\Http\Middleware\VerifyCsrfToken::class]);
     }
 
+    // ログインページを表示するテスト
     public function testShowLoginForm()
     {
         $response = $this->get(route('login'));
@@ -36,6 +39,7 @@ class AuthenticationTest extends TestCase
         $response->assertStatus(200);
     }
 
+    // ログインのテスト
     public function testLogin(): void
     {
         $response = $this->post(route('login', [
@@ -46,6 +50,7 @@ class AuthenticationTest extends TestCase
         $response->assertRedirect(route('templates.index'));
     }
 
+    // ログイン時、パスワードが間違えたときの処理のテスト
     public function testLoginWithWrongPassword()
     {
         $response = $this->post(route('login', [
@@ -56,6 +61,27 @@ class AuthenticationTest extends TestCase
         $response->assertSessionHasErrors(['email' => trans('error_message.email_invalid')]);
     }
 
+    // 認証されていないユーザーが /templates にアクセスした場合のテスト.
+    public function testGuestUserCannotAccessTemplates()
+    {
+        // 認証されていない状態で /templates にアクセス
+        $response = $this->get(route('templates.show'));
+
+        // ログインページへリダイレクトされることを確認
+        $response->assertRedirect(route('login'));
+    }
+
+    // 認証およびメール認証済みのユーザーが /templates にアクセスできることを確認.
+    public function testVerifiedUserCanAccessTemplates()
+    {
+        // 認証済みかつメール認証済み状態で /templates にアクセス
+        $response = $this->actingAs($this->user)->get(route('templates.show'));
+
+        // 正常なレスポンスが返されることを確認
+        $response->assertStatus(200);
+    }
+
+    // ログアウト処理のテスト
     public function testLogout(): void
     {
         // ユーザーを認証
